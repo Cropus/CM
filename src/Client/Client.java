c8d8c0232ea45cf14778da68bb5dd201a9dbb1de
Cropus <baranovnikita18@gmail.com>
2020-07-04 01:30:55 +0300

    Added authentication and authorization, but it isn't work correct

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,13 +1,23 @@
 package Client;
 
+import Exceptions.LoginExistsException;
+import Units.UID;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Client {
+	static final String driver = "com.mysql.cj.jdbc.Driver";
+	static final String url = "jdbc:mysql://localhost:9999/cm?serverTimezone=UTC";
+	static final String login = "root";
+	static final String password = "root";
+	Scanner sc = new Scanner(System.in);
+	String str = "";
 	private BufferedReader in;
 	private PrintWriter out;
 	private Socket socket;
@@ -20,13 +30,11 @@ public class Client {
 			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			out = new PrintWriter(socket.getOutputStream(), true);
 
-			System.out.println("Enter your name");
-			out.println(scan.nextLine());
-
+			sign();
 			Resender resend = new Resender();
 			resend.start();
 
-			String str = "";
+
 			while (!str.equals("exit")) {
 				str = scan.nextLine();
 				out.println(str);
@@ -45,7 +53,7 @@ public class Client {
 			out.close();
 			socket.close();
 		} catch (Exception e) {
-			System.err.println("��⮪� �� �뫨 �������!");
+//TODO log
 		}
 	}
 
@@ -64,23 +72,21 @@ public class Client {
 					System.out.println(str);
 				}
 			} catch (IOException e) {
-				System.err.println("�訡�� �� ����祭�� ᮮ�饭��.");
-				e.printStackTrace();
+//TODO log
 			}
 		}
 	}
 
 
 	void sign() {
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome!");
-		System.out.print("1) Login\n2) Register\nEnter number: ");
+		System.out.print("1) Sign In\n2) Sign Up\nEnter number: ");
 		try {
 			int ans = sc.nextInt();
 			if (ans == 1) {
-				login();
+				signIn();
 			} else if (ans == 2) {
-				register();
+				signUp();
 			} else {
 				throw new IllegalArgumentException();
 			}
@@ -90,11 +96,58 @@ public class Client {
 
 	}
 
-	void login() {
-
+	void signIn() {
+		System.out.println("Start Signing In!");
+		System.out.print("Enter login: ");
+		String newLogin = sc.nextLine();
+		System.out.print("Enter password: ");
+		String newPassword = sc.nextLine();
+		try {
+			Class.forName(driver);
+			Connection connection = DriverManager.getConnection(url, login, password);
+			String checkCredential = "SELECT * FROM users WHERE Login = ? AND Password = ?";
+			PreparedStatement stmt = connection.prepareStatement(checkCredential);
+			stmt.setString(1, newLogin);
+			stmt.setString(2, newPassword);
+			ResultSet rs = stmt.executeQuery();
+			if (rs.next()) {
+				System.out.println("You are logged in!!!");
+			} else {
+				str = "exit";
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
-	void register() {
-
+	void signUp() {
+		System.out.print("Enter login: ");
+		String newLogin = sc.nextLine();
+		System.out.print("Enter password: ");
+		String newPassword = sc.nextLine();
+//		System.out.print("Enter nickname: ");
+//		String nickname = sc.nextLine();
+		try {
+			Class.forName(driver);
+			Connection connection = DriverManager.getConnection(url, login, password);
+			String checkLogin = "SELECT * FROM users WHERE Login = ?";
+			PreparedStatement stmt = connection.prepareStatement(checkLogin);
+			stmt.setString(1, newLogin);
+			ResultSet rs = stmt.executeQuery();
+			if (!rs.next()) {
+				String newUser = "INSERT INTO users (ID, Login, Password, Hash) VALUES (?, ?, ?, ?)";
+				PreparedStatement userStmt = connection.prepareStatement(newUser);
+				userStmt.setInt(1, UID.generate());
+				userStmt.setString(2, newLogin);
+				userStmt.setString(3, newPassword);
+				userStmt.setInt(4, (newLogin + newPassword).hashCode());
+				userStmt.execute();
+				signIn();
+			} else throw new LoginExistsException();
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		} catch (LoginExistsException e) {
+//TODO log
+		}
 	}
 }
---
+++ src/Exceptions/LoginExistsException.java
@@ -0,0 +1,4 @@
+package Exceptions;
+
+public class LoginExistsException extends Exception {
+}
